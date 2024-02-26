2cecd94252d16fd4e1055be47e98bc2c336a7e87
Ronick2005 <ronickaakshath0614@gmail.com>
2024-02-26 09:02:31 +0530

    Updated

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿
-using System;
+﻿using System;
 public class Pattern
 {
     public static void Main()
     {
-        // type the code here
+        int rows, c = 1, a, i, j, n;
+        Console.Write("Enter the no of rows: ");
+        rows = Convert.ToInt32(Console.ReadLine());
+        for (i = 0; i < rows; i++)
+        {
+            for (n = 1; n <= rows - i; n++)
+            {
+                Console.Write(" ");
+            }
+            for (j = 0; j <= i; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    c = 1;
+                }
+                else
+                {
+                    c = c * (i - j + 1) / j;
+                }
+                Console.Write("{0} ", c);
+            }
+            Console.Write("\n");
+        }
+        Console.ReadLine();
     }
 }
